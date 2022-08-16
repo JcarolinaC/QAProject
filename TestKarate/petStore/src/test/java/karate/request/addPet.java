ed158fd9bfa1352950b9742a52ad54e7b21e4092
jcarito <jcarolinaceballos@gmail.com>
2022-08-16 16:40:43 -0500

    the definition steps are carried out and the add pet feature is carried out, there is a pending conflict to be solved

--- TestKarate/petStore/src/test/java/karate/request/addPet.java
+++ TestKarate/petStore/src/test/java/karate/request/addPet.java
@@ -1,4 +1,13 @@
 package karate.request;
 
+import com.intuit.karate.junit5.Karate;
+
+
 public class addPet {
+
+    @Karate.Test
+    Karate testSample() {
+        return Karate.run("classpath:request/addPet.feature").relativeTo(getClass());
+
+    }
 }
